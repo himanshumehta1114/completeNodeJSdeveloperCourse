bd59596eefeee2ef498950c38479550bb9668277
himanshu <himanshumehta1114@gmail.com>
2017-10-13 16:32:54 +0530

    using yargs for user input in key value pair

--- section 3/notes-node/app.js
+++ section 3/notes-node/app.js
@@ -1,7 +1,22 @@
 console.log('Starting app.js');
 
+const yargs = require('yargs');
 const notes = require('./notes.js');
+
+//getting user input in key:value pair by using yargs eg. --title=secrets ,then title is key and value is secrets
+const argv = yargs.argv;
 
 // getting user input at command line by using process.argv
 var command = process.argv[2];
-console.log(command);
+console.log('Yargs: ',argv);
+if(command === 'add'){
+  notes.addNote(argv.title,argv.body);
+}else if (command === 'list') {
+  notes.getAll();
+}else if(command === 'read'){
+  notes.getNote(argv.title);
+}else if(command === 'remove'){
+  notes.removeNote(argv.title);
+}else{
+  console.log('Command not recognised');
+}
