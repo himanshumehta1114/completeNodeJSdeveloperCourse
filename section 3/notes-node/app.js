94966a1b98cfba54e1f013ecda93483353b476b8
himanshu <himanshumehta1114@gmail.com>
2017-10-13 17:47:46 +0530

    Refractoring for Reusability

--- section 3/notes-node/app.js
+++ section 3/notes-node/app.js
@@ -10,7 +10,15 @@ const argv = yargs.argv;
 var command = process.argv[2];
 console.log('Yargs: ',argv);
 if(command === 'add'){
-  notes.addNote(argv.title,argv.body);
+  var note = notes.addNote(argv.title,argv.body);
+  if(note){
+    console.log('Note Created');
+    console.log('--');
+    console.log('Title: ' + note.title);
+    console.log(`Body: ${note.body}`);
+  }else{
+    console.log('Note title taken');
+  }
 }else if (command === 'list') {
   notes.getAll();
 }else if(command === 'read'){
--- section 3/notes-node/notes.js
+++ section 3/notes-node/notes.js
@@ -1,23 +1,32 @@
   console.log('Starting notes.js');
   const fs = require('fs');
+
+  var fetchNotes = () => {
+  try{
+  var notesString = fs.readFileSync('notes-data.json');
+  return JSON.parse(notesString);
+}catch(e) {
+  return [];
+}
+  };
+
+  var saveNotes = (notes) => {
+
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  };
+
   var addNote = (title, body) => {
-    var notes = [];
+    var notes = fetchNotes();
     var note = {
       title,
       body
     }
-    try{
-    var notesString = fs.readFileSync('notes-data.json');
-    notes = JSON.parse(notesString);
-  }catch(e) {
-
-  }
-
   var duplicateNotes = notes.filter((note) => note.title === title);
 
   if( duplicateNotes.length === 0) {
     notes.push(note);
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    saveNotes(notes);
+    return note;
   }
   };
 
