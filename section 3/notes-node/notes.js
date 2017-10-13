dde40108bd455d2e5caabc4894ad7506dd9dbafa
himanshu <himanshumehta1114@gmail.com>
2017-10-13 17:13:40 +0530

    Adding notes to external file if no duplicates are present

--- section 3/notes-node/notes.js
+++ section 3/notes-node/notes.js
@@ -1,7 +1,24 @@
   console.log('Starting notes.js');
-
+  const fs = require('fs');
   var addNote = (title, body) => {
-    console.log('Adding note',title, body);
+    var notes = [];
+    var note = {
+      title,
+      body
+    }
+    try{
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  }catch(e) {
+
+  }
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if( duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
   };
 
   var getAll = () => {
