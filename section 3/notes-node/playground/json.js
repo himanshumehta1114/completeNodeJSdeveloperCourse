70f6e947e4a216ab7ba9ab0c9142728affc68906
himanshu <himanshumehta1114@gmail.com>
2017-10-13 16:42:54 +0530

    parsing object from string

--- section 3/notes-node/playground/json.js
+++ section 3/notes-node/playground/json.js
@@ -1,6 +1,13 @@
-var obj = {
-  name: 'Himanshu'
-};
-var stringObj = JSON.stringify(obj);
-console.log(typeof stringObj);
-console.log(stringObj);
+// var obj = {
+//   name: 'Himanshu'
+// };
+// var stringObj = JSON.stringify(obj);
+// // checking datatype
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+var personString = '{"name": "Himanshu","age": 20}';
+// parsing json from string
+var person = JSON.parse(personString);
+console.log(typeof person);
+console.log(person);
