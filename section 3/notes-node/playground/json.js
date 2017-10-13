b21d4fa4ef42a606658418ae228b609f1064429e
himanshu <himanshumehta1114@gmail.com>
2017-10-13 16:52:55 +0530

    Writing and reading files using fs

--- section 3/notes-node/playground/json.js
+++ section 3/notes-node/playground/json.js
@@ -5,9 +5,23 @@
 // // checking datatype
 // console.log(typeof stringObj);
 // console.log(stringObj);
+//
+// var personString = '{"name": "Himanshu","age": 20}';
+// // parsing json from string
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
 
-var personString = '{"name": "Himanshu","age": 20}';
-// parsing json from string
-var person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+};
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json',originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
