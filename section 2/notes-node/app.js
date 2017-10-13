2b93b570446c58706af4b264a67ef315bcb5b999
himanshu <himanshumehta1114@gmail.com>
2017-10-13 14:56:14 +0530

    Creating sample module

--- section 2/notes-node/app.js
+++ section 2/notes-node/app.js
@@ -4,9 +4,10 @@ const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
 
-var user = os.userInfo();
-console.log(user);
-
-fs.appendFile('greetings.txt', 'Hello ' + user.username + '\n');
-/* using template strings  */
-fs.appendFile('greetings.txt',`Hello ${user.username} World!`);
+// var user = os.userInfo();
+// console.log(user);
+var res = notes.addNote();
+console.log(res);
+// fs.appendFile('greetings.txt', 'Hello ' + user.username + '\n');
+//  using template strings
+// fs.appendFile('greetings.txt',`Hello ${user.username} ! You are ${notes.age}`);
--- section 2/notes-node/notes.js
+++ section 2/notes-node/notes.js
@@ -1,1 +1,6 @@
 console.log('Starting node.js');
+// Using arrow functions ,we can't bind this keyword
+module.exports.addNote = () => {
+    console.log('AddNote');
+    return 'Add Note';
+}
