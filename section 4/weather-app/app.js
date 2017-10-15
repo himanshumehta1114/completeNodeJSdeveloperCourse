62806af66711595fce3e9621a47a9a2c30aab969
himanshu <himanshumehta1114@gmail.com>
2017-10-15 16:32:04 +0530

    Pretty printing objects

--- section 4/weather-app/app.js
+++ section 4/weather-app/app.js
@@ -4,5 +4,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
   json: true
 }, (error,response,body)=>{
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
