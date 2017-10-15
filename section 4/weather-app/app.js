5fa1bdfcb0f9be119e98634aa579ebbb993f1900
himanshu <himanshumehta1114@gmail.com>
2017-10-15 18:54:48 +0530

    callback errors

--- section 4/weather-app/app.js
+++ section 4/weather-app/app.js
@@ -18,8 +18,14 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.a)}`,
   json: true
 }, (error,response,body)=>{
+  if(error){
+    console.log('Unable to connect to google servers');
+  }else if(body.status === 'ZERO_RESULTS'){
+    console.log('unable to find that address');
+  }else if(body.status === 'OK'){
   // here body refers to body of jSON page ie. all information being displayed on browser in json
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
   console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+}
 });
