4c8fa183b7a391d7d3457a662b4e730f1b4bc731
himanshu <himanshumehta1114@gmail.com>
2017-10-18 11:22:21 +0530

    Using dark sky api

--- section 4/weather-app/app.js
+++ section 4/weather-app/app.js
@@ -1,26 +1,35 @@
+// const request = require('request');
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode');
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help','h')
+//   .argv;
+//
+// geocode.getAddress(argv.a, (errorMessage,results) => {
+//   if(errorMessage){
+//     console.log(errorMessage);
+//   }else{
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
 const request = require('request');
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help','h')
-  .argv;
 
-geocode.getAddress(argv.a, (errorMessage,results) => {
-  if(errorMessage){
-    console.log(errorMessage);
-  }else{
-    console.log(JSON.stringify(results, undefined, 2));
-  }
+request({
+  url: 'https://api.darksky.net/forecast/877712e97548129a36678674d3ade8f3/29.136086,75.74104100000001',
+  json: true
+}, (error,response,body) => {
+  console.log(body.currently.temperature);
 });
+
 // request({
 //   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedURI}`,
 //   json: true
