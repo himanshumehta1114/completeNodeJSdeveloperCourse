bdd362139c56e684c065ae2ccf4430010884981c
himanshu <himanshumehta1114@gmail.com>
2017-10-18 13:11:11 +0530

    chaining promsises

--- section 4/weather-app/playground/promise.js
+++ section 4/weather-app/playground/promise.js
@@ -1,13 +1,37 @@
-var somePromise = new Promise((resolve,reject) => {
- setTimeout(() => {
-// only one statement is execued either resolve or reject
-   resolve('Hey it worked');
-  //  reject('Unable to fulfill promise');
- },2500);
+var asyncAdd = (a,b) => {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      if(typeof a === 'number' && typeof b === 'number'){
+        resolve(a+b);
+      }else {
+        reject('Arguments must be number');
+      }
+    }, 1500);
+  });
+};
+
+asyncAdd(5,7).then((res) => {
+  console.log('result : ' + res);
+  return asyncAdd(res, 33);
+}, (errorMessage) => {
+  console.log('error : ' + errorMessage);
+}).then((res) => {
+  console.log('result : ' + res);
+} , (errorMessage) => {
+  console.log(errorMessage);
 });
 
-somePromise.then((message) => {
-  console.log('message :',message);
-}, (errorMessage) => {
-  console.log('Error: ', errorMessage);
-});
+
+// var somePromise = new Promise((resolve,reject) => {
+//  setTimeout(() => {
+// // only one statement is execued either resolve or reject
+//    resolve('Hey it worked');
+//   //  reject('Unable to fulfill promise');
+//  },2500);
+// });
+//
+// somePromise.then((message) => {
+//   console.log('message :',message);
+// }, (errorMessage) => {
+//   console.log('Error: ', errorMessage);
+// });
