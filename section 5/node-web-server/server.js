7de73664f9c1466445332e93bceb8235a86f705b
himanshu <himanshumehta1114@gmail.com>
2017-10-20 16:39:30 +0530

    Using middleware|express.static

--- section 5/node-web-server/server.js
+++ section 5/node-web-server/server.js
@@ -2,8 +2,28 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (request, response) => {
-  response.send('hello Express!');
+  // response.send('<h1>hello Express!</h1>');
+  response.send({
+    name : 'Himanshu Mehta',
+    age: 20,
+    skills : [
+      'C++',
+      'HTML',
+      'CSS',
+      'JAVASCRIPT'
+    ]
+  });
+});
+
+app.get('/about', (request,response) => {
+  response.send('About Page');
+});
+
+app.get('/bad', (request,response) => {
+  response.send({errorMessage : 'Unable to handle request'});
 });
 
 app.listen(3000);
