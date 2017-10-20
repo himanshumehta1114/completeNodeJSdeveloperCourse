0921d36864e9647c041ab087df35a054b62da0f9
himanshu <himanshumehta1114@gmail.com>
2017-10-20 11:03:01 +0530

    Used axios

---
+++ section 4/weather-app/app-promise.js
@@ -0,0 +1,14 @@
+const yargs = require('yargs');
+const axios = require('axios');
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help','h')
+  .argv;
