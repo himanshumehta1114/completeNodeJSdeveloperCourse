93f9592e64c76e00d02a696c04c984eed56a91db
himanshu <himanshumehta1114@gmail.com>
2017-10-26 18:38:10 +0530

    Using assertion libraries|EXPECTlibrary

--- section 6/node-tests/utils/utils.test.js
+++ section 6/node-tests/utils/utils.test.js
@@ -1,13 +1,32 @@
+const expect = require('expect');
+
 const utils = require('./utils');
+
 
 it('should add two numbers', () => {
   var res = utils.add(33,11);
-  if(res != 44)
-  throw new Error(`Expected 44 but got ${res}`);
+
+  expect(res).toBe(44).toBeA('number');
+  // if(res != 44)
+  // throw new Error(`Expected 44 but got ${res}`);
 });
 
 it('should square two numbers', () => {
   var sq = utils.square(4);
-  if(sq != 16)
-  throw new Error(`Expected 16 but got ${sq}`);
+  expect(sq).toBe(16).toBeA('number');
+  // if(sq != 16)
+  // throw new Error(`Expected 16 but got ${sq}`);
+});
+
+// should verify first and last names are set
+
+it('should verify first and last names are set properly', ()=>{
+  var name = {
+    firstName : 'hello',
+    lastName : 'world'
+  }
+  var nameVal = utils.setName(name,'Himanshu Mehta');
+
+  // expect(nameVal).toEqual(name);
+  expect(nameVal).toInclude({firstName: 'Himanshu', lastName: 'Mehta'});
 });
